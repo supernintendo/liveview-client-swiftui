4bbed6b41f822fe8bdb16c26666c5320c2dfb94e
May Matyi <may@matyi.net>
2023-03-28 23:23:55 -0700

    Upgrade live_view_native_platform to 0.0.5

--- lib/live_view_native_swift_ui/modifiers/bold.ex
+++ lib/live_view_native_swift_ui/modifiers/bold.ex
@@ -1,6 +1,5 @@
 defmodule LiveViewNativeSwiftUi.Modifiers.Bold do
   use LiveViewNativePlatform.Modifier
 
-  modifier_schema "bold" do
-  end
+  modifier_schema "bold"
 end
--- lib/live_view_native_swift_ui/modifiers/italic.ex
+++ lib/live_view_native_swift_ui/modifiers/italic.ex
@@ -1,6 +1,5 @@
 defmodule LiveViewNativeSwiftUi.Modifiers.Italic do
   use LiveViewNativePlatform.Modifier
 
-  modifier_schema "italic" do
-  end
+  modifier_schema "italic"
 end
